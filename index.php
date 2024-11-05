186b877b5923b29351e40a9a5096c92258374c5b
CodingBrains <codingbrains10@gmail.com>
2024-11-05 23:03:44 +0530

    add and delete last option tag

--- index.php
+++ index.php
@@ -113,6 +113,7 @@ echo "<script>alert('Something went wrong. Please try again.');</script>";
 					<option value="6">6</option>
 					<option value="7">7</option>
 					<option value="8">8</option>
+					
 					</select>
 					</div>
 				</div>
