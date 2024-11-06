10b124adef6eba0e960d3c3707604dc82aa0ccb0
CodingBrains <codingbrains10@gmail.com>
2024-11-06 15:55:01 +0530

    add a file for check rand function

---
+++ randomnumber.php
@@ -0,0 +1,8 @@
+<?php
+$bno = mt_rand(100000000, 9999999999);
+echo "The generated random number is: " . $bno;
+echo "<br>";
+$bno1 = rand(100000000, 9999999999);
+echo "The generated random number is: " . $bno1;
+
+?>
